57de7327e3b031b01c0b1cfe07fed4c5106bba2a
av1ppp <aviparampampam@gmail.com>
2021-10-05 00:12:42 +0500

    add json

---
+++ src/lib.rs
@@ -0,0 +1,84 @@
+use std::fs;
+use std::io::prelude::*;
+use std::path::Path;
+use serde_json;
+
+use serde::{Deserialize, Serialize};
+
+static FILEPATH: &str = "/home/av1ppp/todos";
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ToDo {
+    pub id: i32,
+    pub message: String,
+}
+
+/// Добавление ToDo записи
+pub fn add(todo: ToDo) -> Result<(), String> {
+    let mut todos = get_all()?;
+    todos.push(todo);
+    save_to_file(todos)
+}
+
+/// Получение ToDo записи по id
+// pub fn get_by_id(id: i32) -> ToDo {
+// }
+
+fn get_file_data() -> Result<String, String> {
+    if Path::new(&FILEPATH).exists() {
+        match fs::File::open(&FILEPATH) {
+            Ok(mut v) => {
+                let mut buf = String::new();
+                match v.read_to_string(&mut buf) {
+                    Ok(_) => Ok(buf),
+                    Err(err) => Err(err.to_string())
+                }
+            }
+            Err(err) => Err(err.to_string())
+        }
+
+    } else {
+        match fs::File::create(&FILEPATH) {
+            Ok(mut v) => {
+                let default_value = String::from("{ \"todos\": [] }");
+
+                match v.write_all(default_value.as_bytes()) {
+                    Ok(()) => Ok(default_value),
+                    Err(err) => Err(err.to_string())
+                }
+            }
+            Err(err) => Err(err.to_string())
+        }
+    }
+}
+
+fn save_to_file(todos: Vec<ToDo>) -> Result<(), String> {
+    let json = FileJSON{todos: todos};
+
+    let data = match serde_json::to_string(&json) {
+        Ok(v) => v,
+        Err(err) => return Err(err.to_string())
+    };
+
+    match fs::write(&FILEPATH, data) {
+        Ok(_) => Ok(()),
+        Err(err) => Err(err.to_string())
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct FileJSON {
+    todos: Vec<ToDo>,
+}
+
+/// Получение всех записей ToDo
+pub fn get_all() -> Result<Vec<ToDo>, String> {
+    let file_data = get_file_data()?;
+
+    let json: FileJSON = match serde_json::from_str(file_data.as_str()) {
+        Ok(v) => v,
+        Err(err) => return Err(err.to_string())
+    };
+
+    Ok(json.todos)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,82 @@
-use std::fs::File;
-use std::fs::OpenOptions;
-use std::io::prelude::*;
-use std::error::Error;
-use std::io::{BufRead, BufReader};
-use std::env;
-use std::str;
+extern crate todo;
 
-struct TodoItem {
-    msg: String
-}
-
-static LOG_FILE_PATH: &str = "./logs.txt";
 
 fn main() {
-    let file: File = get_log_file().unwrap();
-
-    let args: Vec<String> = env::args().collect();
-    assert_ne!(args.len(), 1);
-
-    let cmd: &str = &args[1].to_string();
-
-    match cmd {
-        "add" => {
-            assert_ne!(args.len(), 2);
-            let msg: &str = &args[2].to_string();
-            add_log(file, msg).unwrap();
-        }
-
-        "get" => {
-            let logs = get_logs(file);
-            for l in logs {
-                println!("Log: {}", l.msg);
-            }
-        }
-
-        _ => panic!("Unknown command")
+    let todos = todo::get_all().unwrap();
+    for t in todos {
+        println!("{:?}", t);
     }
+
+    todo::add(todo::ToDo{
+        id: 134,
+        message: String::from("hello"),
+    }).unwrap()
 }
 
-/// Add message to logs
-fn add_log(mut file: File, msg: &str) -> std::io::Result<()> {
-    let msg_ = String::from("\n")+msg;
-    file.write_all(msg_.as_bytes())
-}
+// use std::fs::File;
+// use std::fs::OpenOptions;
+// use std::io::prelude::*;
+// use std::error::Error;
+// use std::io::{BufRead, BufReader};
+// use std::env;
+// use std::str;
 
-/// Returns all log messages
-fn get_logs(file: File) -> Vec<TodoItem> {
-    let mut v: Vec<TodoItem> = vec![];
-    for line in BufReader::new(file).lines() {
-        let line = line.unwrap();
-        if line.len() > 0 {
-            v.push(TodoItem{msg: String::from(line)});
-        }
-    }
-    v
-}
+// struct TodoItem {
+//     msg: String
+// }
 
-/// Returns a log file
-fn get_log_file() -> std::io::Result<File> {
-    OpenOptions::new()
-        .append(true)
-        .write(true)
-        .read(true)
-        .create(true)
-        .open(LOG_FILE_PATH)
-}
+// static LOG_FILE_PATH: &str = "./logs.txt";
+
+// fn main() {
+//     let file: File = get_log_file().unwrap();
+
+//     let args: Vec<String> = env::args().collect();
+//     assert_ne!(args.len(), 1);
+
+//     let cmd: &str = &args[1].to_string();
+
+//     match cmd {
+//         "add" => {
+//             assert_ne!(args.len(), 2);
+//             let msg: &str = &args[2].to_string();
+//             add_log(file, msg).unwrap();
+//         }
+
+//         "get" => {
+//             let logs = get_logs(file);
+//             for l in logs {
+//                 println!("Log: {}", l.msg);
+//             }
+//         }
+
+//         _ => panic!("Unknown command")
+//     }
+// }
+
+// /// Add message to logs
+// fn add_log(mut file: File, msg: &str) -> std::io::Result<()> {
+//     let msg_ = String::from("\n")+msg;
+//     file.write_all(msg_.as_bytes())
+// }
+
+// /// Returns all log messages
+// fn get_logs(file: File) -> Vec<TodoItem> {
+//     let mut v: Vec<TodoItem> = vec![];
+//     for line in BufReader::new(file).lines() {
+//         let line = line.unwrap();
+//         if line.len() > 0 {
+//             v.push(TodoItem{msg: String::from(line)});
+//         }
+//     }
+//     v
+// }
+
+// /// Returns a log file
+// fn get_log_file() -> std::io::Result<File> {
+//     OpenOptions::new()
+//         .append(true)
+//         .write(true)
+//         .read(true)
+//         .create(true)
+//         .open(LOG_FILE_PATH)
+// }
