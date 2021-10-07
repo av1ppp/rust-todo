cbd1558f6d9cd94bf79d581d4b3e63eb4fe1d65f
av1ppp <aviparampampam@gmail.com>
2021-10-07 05:35:45 +0500

    add clap lib

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,47 @@
 extern crate todo;
 
+use clap::{App, Arg, SubCommand};
 use std::env;
 
 fn main() {
+    let matches = App::new("My Super Program")
+        .version("1.0")
+        .author("Kevin K. <kbknapp@gmail.com>")
+        .about("Does awesome things")
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Sets a custom config file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("v")
+                .short("v")
+                .multiple(true)
+                .help("Sets the level of verbosity"),
+        )
+        .subcommand(
+            SubCommand::with_name("test")
+                .about("controls testing features")
+                .version("1.3")
+                .author("Someone E. <someone_else@other.com>")
+                .arg(
+                    Arg::with_name("debug")
+                        .short("d")
+                        .help("print debug information verbosely"),
+                ),
+        )
+        .get_matches();
+
+    /*
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
@@ -18,14 +57,12 @@ fn main() {
                 let id = args[2].parse::<i32>().unwrap();
                 let t = todo::get_by_id(id).unwrap();
                 println!("id: {}\nmessage: {}\n", t.id, t.message);
-
             } else {
                 for t in all_todos {
                     println!("id: {}\nmessage: {}\n", t.id, t.message);
                 }
             }
         }
-        
         "add" => {
             if args.len() < 3 {
                 panic!("not enought args");
@@ -39,10 +76,11 @@ fn main() {
             }
 
             let message = args[2].clone();
-            todo::add(todo::ToDo{
-                id: last_id+1,
+            todo::add(todo::ToDo {
+                id: last_id + 1,
                 message: message,
-            }).unwrap();
+            })
+            .unwrap();
         }
 
         "del" => {
@@ -58,15 +96,5 @@ fn main() {
             panic!("command is not defined");
         }
     }
-
-
-    // let todos = todo::get_all().unwrap();
-    // for t in todos {
-    //     println!("{:?}", t);
-    // }
-
-    // todo::add(todo::ToDo{
-    //     id: 134,
-    //     message: String::from("hello"),
-    // }).unwrap()
+    */
 }
